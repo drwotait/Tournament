bec52fa2f289ab126686f4312d99a3c5f2c02747
drwotait <34352193+drwotait@users.noreply.github.com>
2018-10-02 22:46:04 +0200

    Add files via upload

---
+++ test/java/it/drwota/tournament/TournamentApplicationTests.java
@@ -0,0 +1,16 @@
+package it.drwota.tournament;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TournamentApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
